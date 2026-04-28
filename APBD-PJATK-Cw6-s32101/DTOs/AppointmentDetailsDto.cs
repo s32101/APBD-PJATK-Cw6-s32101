0db75ee423247b480637573cd5bec9c49c911f9a
s32101 <s32101>
2026-04-28 16:39:28 +0200

    Appointments dtos

--- APBD-PJATK-Cw6-s32101/DTOs/AppointmentDetailsDto.cs
+++ APBD-PJATK-Cw6-s32101/DTOs/AppointmentDetailsDto.cs
@@ -1,3 +1,9 @@
 namespace APBD_PJATK_Cw6_s32101.DTOs;
 
-class AppointmentDetailsDto;
+record AppointmentDetailsDto : AppointmentShortDto
+{
+    public required string Status { get; set; }
+    public required string Reason { get; set; }
+    public required string InternalNotes { get; set; }
+    public DateTime CreatedAt { get; set; }
+}
--- APBD-PJATK-Cw6-s32101/DTOs/AppointmentListDto.cs
+++ APBD-PJATK-Cw6-s32101/DTOs/AppointmentListDto.cs
@@ -1,4 +1,12 @@
 
 namespace APBD_PJATK_Cw6_s32101.DTOs;
 
-class AppointmentListDto;
+record AppointmentListDto(
+    IEnumerable<AppointmentShortDto> Appointments
+);
+
+record AppointmentShortDto
+{
+    public int Id { get; set; }
+    public DateTime Date { get; set; }
+}
